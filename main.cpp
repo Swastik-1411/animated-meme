b6700533a4954602cf374120b9b706e56535ba76
Swastik-1411 <77712918+Swastik-1411@users.noreply.github.com>
2021-01-20 10:21:48 +0530

    Add files via upload
    persuing my C and C++ basics from unschool.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//C program to swap two numbers
+#include <stdio.h>
+int main()
+{
+int num1=10,num2=20,temp=0;
+temp=num1;
+num1=num2;
+num2=temp;
+printf("num1=%d",num1);
+printf("\nnum2=%d",num2);
+return 0;
+}
